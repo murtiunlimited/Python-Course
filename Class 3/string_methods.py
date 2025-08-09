63483dafb5c6052f531a2dc0cbcf43fee9474823
murtiunlimited <murtaza32004@gmail.com>
2025-08-09 06:09:23 -0700

    Update string_methods.py

--- Class 3/string_methods.py
+++ Class 3/string_methods.py
@@ -3,3 +3,8 @@ text = "Hello World"
 print(text.lower())         # hello world
 print(text.upper())         # HELLO WORLD
 print(text.find("World"))   # 6, finds the index of the first instance of the string
+
+
+first_o = text.find("o")
+second_o = text.find("o", first_o + 1)
+print("Second 'o' at index:", second_o)
