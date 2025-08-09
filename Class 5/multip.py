cf22850f55a1c104205ed168490d6778b131f08e
murtiunlimited <murtaza32004@gmail.com>
2025-08-09 06:40:56 -0700

    Create multip.py

---
+++ Class 5/multip.py
@@ -0,0 +1,7 @@
+# Value Returning function
+def multiply(x, y):
+    return x * y
+
+# First store returned value into variable then print it
+result = multiply(4, 5)
+print(f"The result is: {result}")
