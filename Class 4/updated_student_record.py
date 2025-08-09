4f0c26c982c3a61c088580e5081621e884a56e39
murtiunlimited <murtaza32004@gmail.com>
2025-08-09 06:19:51 -0700

    Create updated_student_record.py

---
+++ Class 4/updated_student_record.py
@@ -0,0 +1,12 @@
+# Initialize dictionary
+student = {
+    "name": "Murtaza",
+    "age": 21,
+    "grade": "F-"
+}
+
+# Update dictionary items
+student["age"] = 11
+student["grade"] = "F+ Mashallah Good Job"
+
+print(student) # Print updated dictionary
