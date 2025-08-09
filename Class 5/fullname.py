ca34045eb03c5fca19dc6cd660bd7597e5aee48a
murtiunlimited <murtaza32004@gmail.com>
2025-08-09 06:41:25 -0700

    Create fullname.py

---
+++ Class 5/fullname.py
@@ -0,0 +1,6 @@
+# Return value for string
+def get_full_name(firstname, lastname):
+    return f"{firstname} {lastname}"
+
+full_name = get_full_name("Murti", "Hussain")
+print(f"Full name: {full_name}")
