7958eeeedf92cef5d8fb86bc302c1da788ccf8ab
murtiunlimited <murtaza32004@gmail.com>
2025-08-09 06:15:56 -0700

    Create list_methods.py

---
+++ Class 3/list_methods.py
@@ -0,0 +1,8 @@
+numbers = [1, 2, 3, 6]
+
+numbers.append(4)    # Add 4 to end of list: [1, 2, 3, 6, 4]
+numbers.pop()        # Removes last item:
+numbers.remove(2)    # Removes first occurrence of 2: [1, 3, 6]
+numbers.sort()       # Sorts the list: [1, 3, 6]
+
+print(numbers)       # Output after any changes or none made
