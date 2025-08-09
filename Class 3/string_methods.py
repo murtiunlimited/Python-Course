123913eda3ad9245986c06c3913d1ef4900668ad
murtiunlimited <murtaza32004@gmail.com>
2025-08-09 06:07:06 -0700

    Create string_methods.py

---
+++ Class 3/string_methods.py
@@ -0,0 +1,5 @@
+text = "Hello World"
+
+print(text.lower())         # hello world
+print(text.upper())         # HELLO WORLD
+print(text.find("World"))   # 6, finds the index of the first instance of the string
