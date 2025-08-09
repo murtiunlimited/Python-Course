9d1672dcd1ae47fee858f3c4b51b7d5a8e2e2fab
murtiunlimited <murtaza32004@gmail.com>
2025-08-09 06:34:22 -0700

    Create amount_of_fruits.py

---
+++ Class 6/amount_of_fruits.py
@@ -0,0 +1,7 @@
+inventory = {'apples': 5, 'oranges': 3, 'bananas': 8}
+
+# Increase inventory count by 1 for each item
+for item in inventory:
+    inventory[item] += 1 #add only 1 to item (int)
+
+print("Updated inventory:", inventory)
