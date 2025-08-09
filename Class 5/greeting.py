4c9038aa7bd452545d0ccc5d66cb943c878d54d9
murtiunlimited <murtaza32004@gmail.com>
2025-08-09 06:39:30 -0700

    Create greeting.py

---
+++ Class 5/greeting.py
@@ -0,0 +1,11 @@
+# Parameter vs Argument
+# Parameter go inside function definition brackets
+# Argument is the actual value you equate the parameter to.
+#e.g Parameter = Argument ; name = "Murtaza"
+
+def greet(name):
+    print(f"Hello, {name}!")
+
+# Calling the function with different arguments
+greet("Murtaza")
+greet("Muhammad")
