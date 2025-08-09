3ed2cb76ec73aa6b391c232ef9895ea82af16b0b
murtiunlimited <murtaza32004@gmail.com>
2025-08-09 06:20:39 -0700

    Create control_flow_intro.py

---
+++ Class 4/control_flow_intro.py
@@ -0,0 +1,12 @@
+score = 85
+print("Your Score:", score)
+
+if score >= 90:
+    print("Grade: A")
+    print("Mashallah beta")
+elif score >= 80:
+    print("Grade: B")
+    print("RIP if your parents are Asian")
+else:
+    print("Grade: (")
+    print("You’re done bro, college isn’t for u")
