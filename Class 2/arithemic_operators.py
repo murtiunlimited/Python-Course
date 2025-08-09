2a17c6aa55345daa1dfcd1840d768524ea30a0ce
murtiunlimited <murtaza32004@gmail.com>
2025-08-09 06:02:15 -0700

    Create arithemic_operators.py

---
+++ Class 2/arithemic_operators.py
@@ -0,0 +1,14 @@
+a = 10
+b = 3
+
+print("a + b =", a + b)
+print("a - b =", a - b)
+print("a * b =", a * b)
+print("a / b =", a / b)   #gives integer rounded down
+print("a // b =", a // b) #gives decimal
+print("a % b =", a % b)
+print("a ** b =", a ** b)
+
+# Unary operator: converts number to negative
+x = 5
+print("Unary -x =", -x)
