23fa22dd9af2dfc3ab1ad2177eb974d4e56526c6
murtiunlimited <murtaza32004@gmail.com>
2025-08-09 06:03:41 -0700

    Create comparision_operators.py

---
+++ Class 2/comparision_operators.py
@@ -0,0 +1,9 @@
+a = 7
+b = 10
+
+print("a == b:", a == b)   # Equal to
+print("a != b:", a != b)   # Not equal to
+print("a < b:", a < b)     # Less than
+print("a > b:", a > b)     # Greater than
+print("a <= b:", a <= b)   # Less than or equal to
+print("a >= b:", a >= b)   # Greater than or equal to
