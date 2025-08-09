4ca6d6e9e2d72e24a16738ed6e1f0894d1954adc
murtiunlimited <murtaza32004@gmail.com>
2025-08-09 05:52:42 -0700

    Create var_conversion.py

---
+++ var_conversion.py
@@ -0,0 +1,7 @@
+age_str = input("Enter your age: ")
+age_int = int(age_str)
+print("Age as int:", age_int, type(age_int))
+
+height_str = input("Enter your height in meters: ")
+height_float = float(height_str)
+print("Height as float:", height_float, type(height_float))
