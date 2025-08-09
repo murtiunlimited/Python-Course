d5914bec6dc3f73f81a462f41b20ea164390ace8
murtiunlimited <murtaza32004@gmail.com>
2025-08-09 06:18:52 -0700

    Create init_dict.py

---
+++ Class 4/init_dict.py
@@ -0,0 +1,9 @@
+# Initialize a dictionary
+student = {
+    "name": "Murtaza",
+    "age": 21,
+    "grade": "F-"
+}
+
+print(student)           # Print whole dictionary
+print(student["name"])   # Print specific item
